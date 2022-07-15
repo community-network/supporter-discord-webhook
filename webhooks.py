0da177228a997543cbfd2799d8abcb8404e996b7
Zefanja Jobse <zefanjaj@gmail.com>
2022-07-15 23:53:57 +0200

    remove tests

--- webhooks.py
+++ webhooks.py
@@ -50,7 +50,7 @@ async def webhook():
                 "content": "Github sponsor webhooks have now been added."
             }
             async with aiohttp.ClientSession() as session:
-                test = await session.post(f"https://discordapp.com/api/webhooks/{webhook_id}/{webhook_auth}", data=send_json)
+                await session.post(f"https://discordapp.com/api/webhooks/{webhook_id}/{webhook_auth}", data=send_json)
                 return "Setup complete! You will now receive notifications when someone supports you on github!"
         else:
             return e
